ce374a02f3813d7e7e3d4106c726606b8cfdaa16
sabirzhan <albuwka@gmail.com>
2019-05-11 00:50:41 +0600

    SpringSecure

--- src/main/java/sprint/spring/exchange/bootstrap/Bootstrap.java
+++ src/main/java/sprint/spring/exchange/bootstrap/Bootstrap.java
@@ -2,16 +2,45 @@ package sprint.spring.exchange.bootstrap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import sprint.spring.exchange.entity.User;
+import sprint.spring.exchange.entity.UserRoles;
 import sprint.spring.exchange.repository.JournalRepository;
+import sprint.spring.exchange.repository.UserRepository;
+import sprint.spring.exchange.repository.UserRolesRepository;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
     @Autowired
     private JournalRepository journalRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserRolesRepository userRolesRepository;
 
     @Override
     public void run(String... args) throws Exception {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    /*    User user1 = new User("user1","user1","user1",
+                passwordEncoder.encode("user1"),"user1",true);
+        userRepository.save(user1);
+
+        User user2 = new User("user2","user2", "user2",
+                passwordEncoder.encode("user2"),"user2",true);
+        userRepository.save(user2);
+
+        User user3 = new User("user3","user3","user3",
+                passwordEncoder.encode("user3"),"user3",false);
+        userRepository.save(user3);
+
+        UserRoles userRoles1 = new UserRoles(user1, "ROLE_ADMIN");
+        userRolesRepository.save(userRoles1);
+
+        UserRoles userRoles2 = new UserRoles(user2, "ROLE_USER");
+        userRolesRepository.save(userRoles2);
+
+        UserRoles userRoles3 = new UserRoles(user3, "ROLE_USER");
+        userRolesRepository.save(userRoles3);*/
     }
 }
--- src/main/java/sprint/spring/exchange/entity/User.java
+++ src/main/java/sprint/spring/exchange/entity/User.java
@@ -8,21 +8,37 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String fio;
-    private String login;
-    private String password;
+    @Column(name="email")
     private String email;
+    @Column(name="fio")
+    private String fio;
+    @Column(name="login")
+    private String login;
+    @Column(name="password")
+    private String password;
+    @Column(name="phone")
     private String phone;
+    @Column(name="is_active")
+    private boolean isActive;
 
     public User() {
     }
 
-    public User(String fio, String login, String password, String email, String phone) {
+    public User(String email,String fio, String login, String password,  String phone,boolean isActive) {
         this.fio = fio;
         this.login = login;
         this.password = password;
         this.email = email;
         this.phone = phone;
+        this.isActive=isActive;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
     }
 
     public Long getId() {
