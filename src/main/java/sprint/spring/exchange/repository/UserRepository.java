440fe710b7fa3db0b9183e374e8b3f3effaa0d65
sabirzhan <albuwka@gmail.com>
2019-05-11 00:51:32 +0600

    SpringSecure

---
+++ src/main/java/sprint/spring/exchange/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package sprint.spring.exchange.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sprint.spring.exchange.entity.User;
+
+public interface UserRepository extends JpaRepository <User,Long> {
+}
