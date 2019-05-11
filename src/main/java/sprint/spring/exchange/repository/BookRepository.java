69e665b2d3ecec85abcfb6233236e6e90de52579
Nurigami <altyn.nurzat@gmail.com>
2019-05-12 00:42:26 +0600

    added search for Books by name and author

--- src/main/java/sprint/spring/exchange/repository/BookRepository.java
+++ src/main/java/sprint/spring/exchange/repository/BookRepository.java
@@ -3,15 +3,15 @@ package sprint.spring.exchange.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import sprint.spring.exchange.entity.Book;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-
-
-    @Query("SELECT b FROM Book b WHERE lower(b.name) LIKE coalesce(CONCAT('%',:name,'%'),b.name) " +
-            "and lower(b.author) LIKE coalesce(CONCAT('%',:author,'%'),b.author)")
+    @Query("SELECT b FROM Book b WHERE lower(b.name) LIKE coalesce(cast(CONCAT('%',:name,'%') as text),b.name) " +
+            "and lower(b.author) LIKE coalesce(cast(CONCAT('%',:author,'%') as text),b.author)")
     List<Book> findBooksByNameAndAuthor(@Param("name") String name,@Param("author") String author);
 }
